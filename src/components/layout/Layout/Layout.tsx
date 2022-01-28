fc962c6bbe4cfa6ef3f3c2c2af3846d3e4b19373
jnorberto23 <joaovictor.norberto@outlook.com>
2022-01-28 15:44:12 -0300

    feat: created Layout Component

---
+++ src/components/layout/Layout/Layout.tsx
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+
+import FontStyles from "../../../global/FontStyles";
+
+import MainNavigation from "../MainNavigation";
+import { StyledContainer } from "./styled";
+
+
+const Layout = (props: { children: any }) => {
+  return (
+    <Fragment>
+      <FontStyles />
+      <MainNavigation />
+      <StyledContainer>{props.children}</StyledContainer>
+    </Fragment>
+  );
+};
+
+export default Layout;
